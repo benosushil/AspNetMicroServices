71e6efaf46603f514b2f4321bcf07f7a57695267
benosushil <benosushil@gmail.com>
2022-01-18 01:11:27 +0530

    Catalog.Api DB Layer

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,80 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context
+                .Products
+                .Find(x => x.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            /*  return await _context
+                  .Products
+                  .Find(x => x.Category == categoryName)
+                  .ToListAsync();*/
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+
+            /*return await _context
+                .Products
+                .Find(x => x.Name == name)
+                .ToListAsync();*/
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateProduct(Product product)
+        {
+            //_context.Products.
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IProductRepository
+    {
+
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
